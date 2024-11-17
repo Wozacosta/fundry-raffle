cc8ccbc40358f7eebac474b2565727068c4e831e
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-11-18 00:59:00 +0100

    init

---
+++ src/Raffle.sol
@@ -0,0 +1,49 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+//
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+/**
+ * @title A simple Raffle contract
+ * @author Wozacosta
+ * @notice  This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enter() public payable {}
+
+    function pickWinner() public {
+        // Pick a winner
+    }
+
+    /** Getter Functions */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
