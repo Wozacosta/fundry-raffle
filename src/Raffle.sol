7fea46eb04371b7f28b98eb13f755ae41363c939
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-11-18 08:41:52 +0100

    feat: error and event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,13 +30,26 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle_SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enter() public payable {}
+    function enter() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {
         // Pick a winner
